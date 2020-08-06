1fe63b2ed1d4cd72c7781d08b812ba9ddbb02fef
hrushikesh1204@gmail.com <ubuntu@ip-172-31-27-40.us-east-2.compute.internal>
2020-08-06 10:41:03 +0000

    added .... and README

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
-valid=true
-count=1
-while [ $valid ]
-do
-echo $count
-if [ $count -eq 5 ];
-then
-break
+!/bin/bash
+
+echo "Enter username"
+read username
+echo "Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "secret" ) ]]; then
+echo "valid user"
+else
+echo "invalid user"
 fi
-((count++))
-done
+
